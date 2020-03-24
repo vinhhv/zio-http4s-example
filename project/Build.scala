f054d1382ccc631527a5c4549fb0bc07bd6af0e5
Vinh Vu <kroomey@gmail.com>
2020-03-23 23:48:29 -0700

    Add doobie dependency

--- project/Build.scala
+++ project/Build.scala
@@ -4,6 +4,7 @@ import sbt.Keys._
 object Build {
   object v {
     // common
+    val doobie     = "0.8.8"
     val enumeratum = "1.5.13"
     val http4s     = "0.21.1"
     val zio        = "1.0.0-RC18-2"
@@ -19,6 +20,8 @@ object Build {
     , "dev.zio"      %% "zio"                 % v.zio
     , "dev.zio"      %% "zio-macros-core"     % v.zioMacros
     , "dev.zio"      %% "zio-macros-test"     % v.zioMacros
+    , "org.tpolecat" %% "doobie-core"         % v.doobie
+    , "org.tpolecat" %% "doobie-h2"           % v.doobie
     , "org.http4s"   %% "http4s-blaze-server" % v.http4s
     , "org.http4s"   %% "http4s-circe"        % v.http4s
     , "org.http4s"   %% "http4s-dsl"          % v.http4s
